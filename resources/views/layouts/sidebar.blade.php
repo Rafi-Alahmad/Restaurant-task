13ce2331dd0376cdbc72c630357613046745014e
rafi <rafi@mail.com>
2022-01-19 23:07:45 +0300

    build dashboard structure

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,23 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{route('settings')}}">
+                    <span data-feather="home"></span>
+                    <i class="mx-1 fas fa-cogs "></i>
+                    {{trans('app.settings')}}
+                </a>
+            </li>
+            <li class="nav-item mt-3">
+                <hr style="height: 0.1px; " class="mx-3 my-2">
+                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="mx-1 fas fa-sign-out-alt"></i>
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+    </div>
+</nav>
